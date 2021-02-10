a2ae3b2c687fa3df3284fa55a0acf9966815109d
Kevin Zhou <kevinzhou22@outlook.com>
2021-02-10 16:49:51 +1100

    Set up index.js to initialize project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './reset.css';
 import './style.css';
+import createGame from './modules/createGame';
+import createDOMController from './modules/createDOMController';
+import events from './modules/events';
+
+const DOMController = createDOMController(createGame(events));
+DOMController.initialize();
